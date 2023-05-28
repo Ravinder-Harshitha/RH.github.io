0c5c69967560a9eeba6bf90aca42ae94d89e5a7d
Ravinder-Harshitha <115135659+Ravinder-Harshitha@users.noreply.github.com>
2023-05-28 18:05:46 +0400

    website

---
+++ Yoga website/Yogic Meditation page .js
@@ -0,0 +1,19 @@
+//Expandable Tabs//
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
+
+  // When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
---
+++ Yoga website/Yogic starting .js
@@ -0,0 +1,55 @@
+//Appearing text
+var i = 0;
+var txt = '" Yoga takes you into the present moment, \nthe only place where life exists. " \n- Patanjali'
+var speed = 50;
+
+function typeWriter() {
+if (i < txt.length) {
+    if (txt.charAt(i) === '\n') {
+    document.getElementById("demo").innerHTML += '<br>';
+    } else {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    }
+    i++;
+    setTimeout(typeWriter, speed);
+}
+}
+window.onload = typeWriter;
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//Testimonials slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
